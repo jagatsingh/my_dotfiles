f7a63bda99c39ce0aadc281e7a3e7e18dba891e5
jj <jj@1204master64bit.(none)>
2014-09-28 13:35:00 +1000

    Sbt eclipse

--- configure.sh
+++ configure.sh
@@ -1,2 +1,9 @@
 DEV_HOME=/home/jj/dev
-ln -s $DEV_HOME/code/open/my_ubuntu/files/profile /home/$USER/.profile
+# Bash profile script
+ln -sf ${DEV_HOME}/code/open/my_dotfiles/files/profile /home/$USER/.profile
+# sbt
+for sbtversion in 0.13 
+do
+	mkdir -p /home/$USER/.sbt/${sbtversion}/plugins
+	ln -sf $DEV_HOME/code/open/my_dotfiles/files/sbt/plugins.sbt /home/$USER/.sbt/${sbtversion}/plugins/plugins.sbt
+done

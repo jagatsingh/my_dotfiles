fe9dc36d4a1394112caf0dd00ff20d312a76c86f
jj <jj@1204master64bit.(none)>
2014-09-28 12:18:03 +1000

    Just initial version

---
+++ apt-install.sh
@@ -0,0 +1,15 @@
+apt-get install apt-cacher-ng
+apt-get install synaptic
+apt-get install buildtools
+apt-get install rabbitvcs-nautilus3 rabbitvcs-thunar rabbitvcs-gedit rabbitvcs-cli thunarax-python
+apt-get install gitg
+apt-get install giggle
+apt-get install searchmonkey
+apt-get install vagrant
+apt-get install virtualbox
+apt-get install init-system-helpers
+apt-get install sysv-rc
+apt-get install file-rc
+apt-get install openssh-server openssh-client
+apt-get install gnome-panel
+apt-get install fbreader
